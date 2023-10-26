8d3ec80d6b9c7a95270bd369fda4d4799f5c76e7
salinpaul valooran <salinpv@hotmail.com>
2023-10-27 01:56:02 +0530

    dynamically adding to the todo list

--- version01/index.js
+++ version01/index.js
@@ -2,5 +2,12 @@ const input_box = document.getElementById("input-box");
 const list_container = document.getElementById("list-container")
 
 function add_task() {
-
+  if(input_box.value === "") {
+    alert("Enter a valid task")
+  } 
+  else {
+    let li = document.createElement("li");
+    li.innerHTML = input_box.value;
+    list_container.appendChild(li);
+  }
 }
